fe6232c58ec18c414be5e320f67d13f031513618
UltraLionFr <ultralionfr@gmail.com>
2025-12-23 07:57:00 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,31 @@ const nextConfig: NextConfig = {
                 source: "/:path*",
                 headers: [
                     { key: "X-DNS-Prefetch-Control", value: "on" },
-                    { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=63072000; includeSubDomains; preload",
+                    },
                     { key: "X-Content-Type-Options", value: "nosniff" },
                     { key: "X-Frame-Options", value: "DENY" },
                     { key: "X-XSS-Protection", value: "1; mode=block" },
-                    { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-                    { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
+                    {
+                        key: "Referrer-Policy",
+                        value: "strict-origin-when-cross-origin",
+                    },
+                    {
+                        key: "Permissions-Policy",
+                        value: "camera=(), microphone=(), geolocation=()",
+                    },
                 ],
+            },
+        ];
+    },
+
+    async rewrites() {
+        return [
+            {
+                source: "/waitlist/en",
+                destination: "/en", // Affiche le contenu de /en tout en gardant l'URL /waitlist/en
             },
         ];
     },
